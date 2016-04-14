7bb5d16729fa2dcd8bd13c7c2aca07f008bbf70d
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-04-14 15:35:30 +0200

    Added before block in spec

--- Customers/spec/party_query_spec.rb
+++ Customers/spec/party_query_spec.rb
@@ -2,27 +2,8 @@ require 'party_query'
 require 'pry'
 
 RSpec.describe PartyQuery, "#query_customers" do
-  it 'invites one customer' do
-    customers = {
-      customers: [
-        {"latitude": "53.2451022", "user_id": 4, "name": "Ian Kehoe", "longitude": "-6.238335"},
-        {"latitude": "55.033", "user_id": 19, "name": "Enid Cahill", "longitude": "-8.112"}
-      ]
-    }
-
-    expected_invited_customers = {
-      customers: [
-        {"user_id": 4, "name": "Ian Kehoe"}
-      ]
-    }
-
-    party_query = PartyQuery.new(customers)
-    party_query.query_customers(100)
-    expect(party_query.invited_customers).to eq expected_invited_customers
-  end
-
-  it 'sorts by user id' do
-    customers = {
+  before :each do
+    @customers = {
       customers: [
         {"latitude": "53.2451022", "user_id": 4, "name": "Ian Kehoe", "longitude": "-6.238335"},
         {"latitude": "55.033", "user_id": 19, "name": "Enid Cahill", "longitude": "-8.112"},
@@ -30,15 +11,23 @@ RSpec.describe PartyQuery, "#query_customers" do
       ]
     }
 
-    expected_invited_customers = {
+    @expected_invited_customers = {
       customers: [
         {"user_id": 4, "name": "Ian Kehoe"},
         {"user_id": 11, "name": "Richard Finnegan"}
       ]
     }
+  end
 
-    party_query = PartyQuery.new(customers)
+  it 'invites one customer' do
+    party_query = PartyQuery.new(@customers)
     party_query.query_customers(100)
-    expect(party_query.invited_customers).to eq expected_invited_customers
+    expect(party_query.invited_customers.size).to eq 1
+  end
+
+  it 'sorts by user id' do
+    party_query = PartyQuery.new(@customers)
+    party_query.query_customers(100)
+    expect(party_query.invited_customers).to eq @expected_invited_customers
   end
 end
